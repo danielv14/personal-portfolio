b18e243edd5b73b27c5a29f52dfed8c436721646
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-27 22:45:43 +0200

    add button primary component

---
+++ src/components/ui/buttons/ButtonPrimary.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ButtonPrimary = styled.div`
+  background: var(--primary-color);
+  color: white;
+  padding: 16px 20px;
+  text-align: center;
+  font-weight: bold;
+  border-radius: var(--border-radius);
+  box-shadow: 0px 4px 7px rgba(102, 145, 255, 0.35);
+  transition: all 0.15s ease;
+  cursor: pointer;
+  width: auto;
+  display: inline-block;
+  &:hover {
+    transform: translateY(-1px);
+    background: var(--primary-darken-color);
+    box-shadow: 0px 4px 10px rgba(102, 145, 255, 0.35);
+  }
+`;
+
+ButtonPrimary.displayName = 'ButtonPrimary';
