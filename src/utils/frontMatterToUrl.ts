879d038259d473d5fafe93ed5450647a1b28dc48
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-28 18:29:07 +0200

    fix undefied error

--- src/utils/frontMatterToUrl.ts
+++ src/utils/frontMatterToUrl.ts
@@ -1,4 +1,6 @@
 import { PostFrontMatter } from '../types/FrontMatter';
 
-export const toBlogURL = (frontMatter: PostFrontMatter) =>
-  `/blogg/${frontMatter.__resourcePath?.split('/')?.pop()?.replace('.mdx', '')}`;
+export const toBlogURL = (frontMatter: PostFrontMatter) => {
+  const blogPath = frontMatter.__resourcePath.split('/').pop();
+  return `/blogg/${blogPath ? blogPath.replace('.mdx', '') : ''}`;
+};
