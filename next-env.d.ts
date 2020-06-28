a0a85c5384c5405fadbd492f4b68b5e5ba05e2a5
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-28 18:26:18 +0200

    fix mdx declarations

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.mdx' {
+  import { ReactNode } from 'react';
+
+  export const frontMatter: {
+    title: string;
+    layout: string;
+    summary: string;
+    date: string;
+    __resourcePath: string;
+  };
+
+  const component: ReactNode;
+  export default ReactNode;
+}
