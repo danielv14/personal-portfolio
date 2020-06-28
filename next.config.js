1c2251b065fe82fd77d55159282f5391dfd2a47d
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-28 15:29:51 +0200

    add prism plugin to next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withMdxEnhanced = require('next-mdx-enhanced');
+const mdxPrism = require('mdx-prism');
 
 module.exports = withMdxEnhanced({
   layoutPath: 'src/layouts',
   defaultLayout: true,
   fileExtensions: ['mdx'],
   remarkPlugins: [],
-  rehypePlugins: [],
+  rehypePlugins: [mdxPrism],
 })(/* your normal nextjs config */);
