dd41f0453f96f51190a488cabb810b42042c1a5d
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-28 15:18:17 +0200

    add syntax highlighting through prism

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { GlobalStyle } from '../components/ui/globalStyle';
 import { Navbar } from '../components/navbar/navbar';
+import './prism.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
