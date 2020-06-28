c6ca3fb52dec1078acbf60f944ff473959fae513
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-28 15:18:48 +0200

    format for blog post layout

--- src/layouts/blogPost.tsx
+++ src/layouts/blogPost.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { PostFrontMatter } from '../types/FrontMatter';
 import { ResponsiveContainer } from '../components/ui/container/responsiveContainer';
+import { MarginLarge } from '../components/ui/margins/marginLarge';
+import { Column } from '../components/ui/container/column';
+import { MarginMedium } from '../components/ui/margins/marginMedium';
 
 interface PostProps {
   children: React.ReactChildren;
@@ -8,13 +11,14 @@ interface PostProps {
 
 export default (frontMatter: PostFrontMatter) => {
   return ({ children }: PostProps) => {
-    const fronty = frontMatter;
     return (
       <ResponsiveContainer>
-        <div>
-          <p>{fronty.layout}</p>
-        </div>
-        {children}
+        <Column>
+          <MarginLarge />
+          <h1>{frontMatter.title}</h1>
+          <MarginMedium />
+          {children}
+        </Column>
       </ResponsiveContainer>
     );
   };
