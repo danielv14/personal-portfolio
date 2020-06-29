a1ab0f8d442e4df071671052d44b5de97afd3483
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-29 14:24:34 +0200

    emoji favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preload" href="/fonts/Montserrat-Bold.ttf" as="font" crossOrigin="" />
         <link rel="preload" href="/fonts/Montserrat-Regular.ttf" as="font" crossOrigin="" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
+        />
       </Head>
       <GlobalStyle />
       <Navbar />
