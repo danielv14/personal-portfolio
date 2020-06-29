dee05815d3a38b1975174bac4736a84694032e7c
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-29 12:18:24 +0200

    add icons for dark mode toggling

---
+++ src/components/ui/icons/iconSun.tsx
@@ -0,0 +1,10 @@
+import { Sun } from '@styled-icons/feather';
+import styled from 'styled-components';
+
+export const IconSun = styled(Sun)`
+  width: 24px;
+  height: 24px;
+  color: var(--text-muted-color);
+`;
+
+IconSun.displayName = 'IconSun';
