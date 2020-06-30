4ac209f7fe317abc8a3af3a0d5a31ec30841d243
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-06-30 11:20:35 +0200

    added card post component to be used to link between posts

---
+++ src/components/cardPost.tsx
@@ -0,0 +1,33 @@
+import { PostMetaData } from '../types/FrontMatter';
+import { Card } from './ui/container/card';
+import Link from 'next/link';
+import { Row } from './ui/container/row';
+import { MarginSmall } from './ui/margins/marginSmall';
+import { Column } from './ui/container/column';
+import { TextMuted } from './ui/content/textMuted';
+
+interface CardPostProps {
+  post: PostMetaData;
+}
+
+const truncate = (text: string) => {
+  return text.length > 40 ? `${text.slice(0, 40)}...` : text;
+};
+
+export const CardPost: React.FC<CardPostProps> = ({ post }) => (
+  <Card style={{ cursor: 'pointer' }}>
+    <Link href={post.url}>
+      <Row style={{ height: '100%' }}>
+        <MarginSmall />
+        <Column style={{ alignItems: 'stretch' }}>
+          <MarginSmall />
+          <h3 style={{ marginTop: '0' }}>{truncate(post.title)}</h3>
+          <TextMuted>{post.date}</TextMuted>
+        </Column>
+        <MarginSmall />
+      </Row>
+    </Link>
+  </Card>
+);
+
+CardPost.displayName = 'CardPost';
