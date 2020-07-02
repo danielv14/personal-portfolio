239c1ce9512262f6b0050e3036f8f9e18e125c96
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-07-02 19:13:22 +0200

    add mail icon and refactor icon style composition to reduce duplicated styles

--- src/components/ui/icons/iconMail.tsx
+++ src/components/ui/icons/iconMail.tsx
@@ -1,7 +1,7 @@
-import { Github } from '@styled-icons/feather';
+import { Mail } from '@styled-icons/feather';
 import styled from 'styled-components';
 import { BaseStyle, IconProps } from './iconBase';
 
-export const IconGitHub = styled(Github)<IconProps>`
+export const IconMail = styled(Mail)<IconProps>`
   ${BaseStyle}
 `;
