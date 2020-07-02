692d3ef72bc07b37fb9f5eb6691447d5cf50d52b
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-07-02 15:34:09 +0200

    update toolbox item

--- src/data/toolbox.ts
+++ src/data/toolbox.ts
@@ -6,7 +6,7 @@ interface ToolboxCategory {
 export const toolBox: ToolboxCategory[] = [
   {
     title: 'Nära till hands',
-    items: ['HTML5', 'CSS och Preprocessors', 'TypeScript', 'React och Next.js', 'Node', 'Git', 'Jest'],
+    items: ['HTML5', 'CSS och Preprocessors', 'JavaScript och TypeScript', 'React och Next.js', 'Node', 'Git', 'Jest'],
   },
   { title: 'Använder gärna', items: ['NoSQL med Firebase', 'PostgreSQL', 'Sketch och Figma', 'Vue.js'] },
 ];
