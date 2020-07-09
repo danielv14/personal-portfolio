84fb143915578078ec31fc0e498d56c11e7484c1
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-07-09 18:01:46 +0200

    fix hover state on mobile for post list items (#30)
    * Fix paddings on mobile for list items

--- src/components/postListItem/postListItem.styles.ts
+++ src/components/postListItem/postListItem.styles.ts
@@ -8,6 +8,7 @@ export const ItemHeader = styled.h4`
 export const Item = styled.div`
   cursor: pointer;
   border-radius: var(--border-radius-small);
+  padding: var(--margin-small);
   transition: all 0.2s ease;
   &:hover {
     background: var(--bg-color-highlight);
