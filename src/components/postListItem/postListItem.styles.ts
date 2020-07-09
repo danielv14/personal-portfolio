a82b1b1edceed243dacdf3a8c777133881c4860a
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-07-09 17:56:30 +0200

    move hover styles into media query since its only relevant for cursors

--- src/components/postListItem/postListItem.styles.ts
+++ src/components/postListItem/postListItem.styles.ts
@@ -8,12 +8,12 @@ export const ItemHeader = styled.h4`
 export const Item = styled.div`
   cursor: pointer;
   border-radius: var(--border-radius-small);
-  transition: all 0.2s ease;
-  &:hover {
-    background: var(--bg-color-highlight);
-    transform: translateY(-2px);
-  }
   @media (min-width: ${theme.breakpoints.medium}) {
+    transition: all 0.2s ease;
     padding: var(--margin-small) var(--margin-large);
+    &:hover {
+      background: var(--bg-color-highlight);
+      transform: translateY(-2px);
+    }
   }
 `;
