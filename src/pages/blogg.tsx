dde2f5dc0411230a9d828ef1e4c64fb838edc902
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-07-19 17:40:27 +0200

    add heading to blog content and give heading block some margins (#42)

--- src/pages/blogg.tsx
+++ src/pages/blogg.tsx
@@ -6,6 +6,8 @@ import { UnstyledInternalLink } from '../components/ui/content/unstyledLink';
 import { MarginLarge } from '../components/ui/margins/marginLarge';
 import { MarginMedium } from '../components/ui/margins/marginMedium';
 import { useContent } from '../context/ContentContext';
+import { HeadingMuted } from '../components/ui/content/headingMuted';
+import { Column } from '../components/ui/container/column';
 
 const blogIndex = () => {
   const { blogPosts } = useContent();
@@ -13,19 +15,24 @@ const blogIndex = () => {
     <>
       <Seo title="Blogg" />
       <ResponsiveContainer>
-        <MarginLarge />
-        <TextCenter>
-          <h1>Alla artiklar</h1>
-        </TextCenter>
-        <MarginLarge />
-        {blogPosts.map((blogPost) => (
-          <UnstyledInternalLink key={blogPost.title} href={blogPost.url}>
-            <div>
-              <PostListItem {...blogPost} />
-              <MarginMedium />
-            </div>
-          </UnstyledInternalLink>
-        ))}
+        <Column>
+          <MarginLarge />
+          <TextCenter>
+            <h1>Alla inlägg</h1>
+            <HeadingMuted as="h5">
+              Då och då skriver jag ned några tankar, dokumenterar upptäckter eller skriver enklare guider.
+            </HeadingMuted>
+          </TextCenter>
+          <MarginLarge />
+          {blogPosts.map((blogPost) => (
+            <UnstyledInternalLink key={blogPost.title} href={blogPost.url}>
+              <div>
+                <PostListItem {...blogPost} />
+                <MarginMedium />
+              </div>
+            </UnstyledInternalLink>
+          ))}
+        </Column>
       </ResponsiveContainer>
     </>
   );
