7ab8446c0ec7a0c941dcfe9d92745d3cadb4874f
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-07-24 17:19:03 +0200

    make card a motion.div

--- src/components/ui/container/card.tsx
+++ src/components/ui/container/card.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const Card = styled.div`
+export const Card = styled(motion.div)`
   border-radius: var(--border-radius);
   background: var(--bg-color-secondary);
   padding: var(--margin-small);
   box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.09);
-  transition: all 0.2s ease;
+  transition: box-shadow 0.2s ease;
   &:hover {
-    transform: translateY(-2px);
     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.09);
   }
 `;
