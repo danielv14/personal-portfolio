09a947fce87a4aa669d25e2f7270214aed24eed7
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-07-24 11:54:55 +0200

    Animate waving hand in hero section (#46)
    * add waving hand emoji component
    
    * Skev hero title a little and add waving hand animated emoji
    
    * organize imports
    
    * tweak animation
    
    * move hero title outside of component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import styled from 'styled-components';
 import { CardProject } from '../components/cards/cardProject';
+import { PostListItem } from '../components/postListItem/postListItem';
 import { ToolBox } from '../components/toolbox/toolBox';
 import { ButtonPrimary } from '../components/ui/buttons/ButtonPrimary';
 import { Column } from '../components/ui/container/column';
@@ -6,20 +8,26 @@ import { Fill } from '../components/ui/container/fill';
 import { ResponsiveContainer } from '../components/ui/container/responsiveContainer';
 import { ResponsiveGrid } from '../components/ui/container/responsiveGrid';
 import { Row } from '../components/ui/container/row';
+import { HeadingMuted } from '../components/ui/content/headingMuted';
+import { TextCenter } from '../components/ui/content/textCenter';
+import { TextMuted } from '../components/ui/content/textMuted';
+import { UnstyledInternalLink } from '../components/ui/content/unstyledLink';
 import { MarginLarge } from '../components/ui/margins/marginLarge';
 import { MarginMedium } from '../components/ui/margins/marginMedium';
-import { TextCenter } from '../components/ui/content/textCenter';
-import { site } from '../data/site';
-import { PostMetaData } from '../types/FrontMatter';
-import { PostListItem } from '../components/postListItem/postListItem';
-import { theme } from '../theme/theme';
-import { HeadingMuted } from '../components/ui/content/headingMuted';
-import { UnstyledInternalLink } from '../components/ui/content/unstyledLink';
-import { useScrollToElement } from '../hooks/useScrollToElement';
-import { TextMuted } from '../components/ui/content/textMuted';
-import { toolboxCategories } from '../data/toolBoxCategories';
+import { WavingHand } from '../components/ui/wavingHand';
 import { useContent } from '../context/ContentContext';
+import { site } from '../data/site';
+import { toolboxCategories } from '../data/toolBoxCategories';
+import { useScrollToElement } from '../hooks/useScrollToElement';
+import { theme } from '../theme/theme';
+import { PostMetaData } from '../types/FrontMatter';
 import { Project } from '../types/Project';
+
+const HeroTitle = styled.h1`
+  display: inline-block;
+  transform: skewY(-4deg);
+  font-style: italic;
+`;
 
 export default function Home() {
   const { blogPosts, projects } = useContent();
@@ -79,7 +87,11 @@ const HeroContent = () => {
     <section>
       <Column>
         <TextCenter>
-          <h1>👋 Hallå där!</h1>
+          <div>
+            <HeroTitle>
+              <WavingHand /> Hallå där!
+            </HeroTitle>
+          </div>
           <HeadingMuted as="h5">
             Daniel här! Webbutvecklare med förkärlek till JavaScript, schysst design och tv-serier.
           </HeadingMuted>
