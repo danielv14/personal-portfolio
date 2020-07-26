fd432b1bea9fb2ef986123a78aecfa8f49d806e8
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-07-26 15:54:00 +0200

    add motion animation to button component with styled.attrs

--- src/components/ui/buttons/ButtonPrimary.tsx
+++ src/components/ui/buttons/ButtonPrimary.tsx
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
+import { motion, MotionProps } from 'framer-motion';
 
-export const ButtonPrimary = styled.div`
+const animation: MotionProps = {
+  whileHover: { y: -1 },
+  whileTap: { y: 2 },
+  transition: {
+    type: 'spring',
+    stiffness: 300,
+    damping: 10,
+  },
+};
+
+export const ButtonPrimary = styled(motion.div).attrs({ ...animation })`
   background: var(--primary-color);
   color: white;
   padding: 16px 20px;
@@ -8,12 +19,11 @@ export const ButtonPrimary = styled.div`
   font-weight: bold;
   border-radius: var(--border-radius);
   box-shadow: var(--box-shadow-button);
-  transition: transform 0.15s ease, background-color 0.15s ease, box-shadow 0.15s ease;
+  transition: background-color 0.2s ease, box-shadow 0.2s ease;
   cursor: pointer;
   width: auto;
   display: inline-block;
   &:hover {
-    transform: translateY(-1px);
     background: var(--primary-color-darken);
     box-shadow: var(--box-shadow-button-hover);
   }
