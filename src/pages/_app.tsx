73edf1109fa7e0a2ba7b07b768e9814a8cb71817
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2020-07-28 15:15:55 +0200

    Remove display:swap to remove jankyness

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,10 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700" rel="stylesheet" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
