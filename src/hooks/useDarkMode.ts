0b27c4045f51b1c69abb9d81ceea10b4f446b3de
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-07-30 20:25:53 +0200

    move theme into global theme colors file

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -2,22 +2,18 @@ import { useState, useEffect } from 'react';
 import { useMedia } from 'use-media';
 import { site } from '../data/site';
 import { storage } from '../utils/storage';
+import { Theme } from '../types/theme/Colors';
 
 const storage_key = `${site.author} - theme`;
 const darkModeClass = 'dark-mode';
 const usePrefersDarkMode = () => useMedia('(prefers-color-scheme: dark)');
-
-enum ThemeState {
-  Dark = 'dark',
-  Light = 'light',
-}
 
 export const useDarkMode = () => {
   const [isDarkMode, toggle] = useState<boolean>(false);
   const prefersDarkMode = usePrefersDarkMode();
   const toggleDarkMode = (): void => {
     toggle(!isDarkMode);
-    storage.set(storage_key, isDarkMode ? ThemeState.Light : ThemeState.Dark);
+    storage.set(storage_key, isDarkMode ? Theme.Light : Theme.Dark);
     document.body.classList.toggle(darkModeClass);
   };
 
@@ -32,7 +28,7 @@ export const useDarkMode = () => {
   }, [prefersDarkMode]);
 
   useEffect(() => {
-    if (storage.get(storage_key) === ThemeState.Dark) {
+    if (storage.get(storage_key) === Theme.Dark) {
       toggleDarkMode();
     }
   }, []);
