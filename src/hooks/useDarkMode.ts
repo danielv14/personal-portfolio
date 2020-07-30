a605a01780ff7fecd37539b3975e95ebb39fdcac
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-07-30 18:08:47 +0200

    set dark mode if prefers-color-scheme is dark only when no storage is set Also set dark or light strings instead of true/false and enumify those values

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,20 +1,38 @@
 import { useState, useEffect } from 'react';
+import { useMedia } from 'use-media';
 import { site } from '../data/site';
 import { storage } from '../utils/storage';
 
-const storage_key = `${site.author} - dark-mode`;
+const storage_key = `${site.author} - theme`;
+const darkModeClass = 'dark-mode';
+const usePrefersDarkMode = () => useMedia('(prefers-color-scheme: dark)');
+
+enum ThemeState {
+  Dark = 'dark',
+  Light = 'light',
+}
 
 export const useDarkMode = () => {
   const [isDarkMode, toggle] = useState<boolean>(false);
-
+  const prefersDarkMode = usePrefersDarkMode();
   const toggleDarkMode = (): void => {
-    storage.set(storage_key, isDarkMode ? 'false' : 'true');
-    document.body.classList.toggle('dark-mode');
     toggle(!isDarkMode);
+    storage.set(storage_key, isDarkMode ? ThemeState.Light : ThemeState.Dark);
+    document.body.classList.toggle(darkModeClass);
   };
 
   useEffect(() => {
-    if (storage.get(storage_key) === 'true') {
+    // If user prefers dark mode and no user stored preference is set
+    // then toggle dark mode to true and add the dark mode class to DOM body
+    // without setting storage pref - only user action through this hook's toggleDarkMode should set to storage
+    if (prefersDarkMode && !storage.has(storage_key)) {
+      toggle(true);
+      document.body.classList.add(darkModeClass);
+    }
+  }, [prefersDarkMode]);
+
+  useEffect(() => {
+    if (storage.get(storage_key) === ThemeState.Dark) {
       toggleDarkMode();
     }
   }, []);
