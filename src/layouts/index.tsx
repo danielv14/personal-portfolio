442180a9a25c7589416f3db7fbdbcf17c5948a82
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-07-31 13:28:44 +0200

    fix seo title

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,18 +3,17 @@ import { FrontMatterBase } from '../types/FrontMatter';
 import { ResponsiveContainer } from '../components/ui/container/responsiveContainer';
 import { MarginLarge } from '../components/ui/margins/marginLarge';
 import { Seo } from '../components/seo/seo';
-import { title } from 'process';
 
 interface DefaultProps {
   children: React.ReactChildren;
 }
 
-const layoutDefault = (_frontMatter: FrontMatterBase) => {
+const layoutDefault = (frontMatter: FrontMatterBase) => {
   // eslint-disable-next-line react/display-name
   return ({ children }: DefaultProps) => {
     return (
       <>
-        <Seo title={title} />
+        <Seo title={frontMatter.title} />
         <ResponsiveContainer>
           <MarginLarge />
           {children}
