f57822b5a99eec6fa183224722cf2353dd2d455b
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-08-04 17:52:21 +0200

    extract mapping of blog post data type into util folder for reuse

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -1,14 +1,7 @@
 import { frontMatter } from '../pages/blogg/*.mdx';
 import { PostFrontMatter, PostMetaData } from '../types/FrontMatter';
+import { toPostMetaData } from '../utils/postMappings';
 import { sortBlogPostsAscByDate } from '../utils/sortBlogPostsByDate';
-import { resourcePathToBlogURL } from '../utils/resourcePathToBlogURL';
-import { formatDate } from '../utils/formatDate';
-
-const toPostMetaData = (frontMatter: PostFrontMatter): PostMetaData => ({
-  ...frontMatter,
-  date: formatDate(frontMatter.date),
-  url: resourcePathToBlogURL(frontMatter.__resourcePath),
-});
 
 export const getAllBlogPosts = (): PostMetaData[] => {
   const blogPosts = (frontMatter as unknown) as PostFrontMatter[];
