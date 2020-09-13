9f761aff51eebb8ce6727ca6b0ca90ad94ba5bf9
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-09-13 21:58:29 +0200

    fix new layout syntax and added shared layout typings file

--- src/layouts/blogPost.tsx
+++ src/layouts/blogPost.tsx
@@ -10,46 +10,39 @@ import { MarginLarge } from '../components/ui/margins/marginLarge';
 import { MarginMedium } from '../components/ui/margins/marginMedium';
 import { usePrevAndNextBlogPost } from '../hooks/usePrevAndNextBlogPost';
 import { theme } from '../theme/theme';
-import { PostFrontMatter } from '../types/FrontMatter';
+import { PostLayoutProps } from '../types/Layout';
 import { toPostMetaData } from '../utils/postMappings';
 
-interface PostProps {
-  children: React.ReactChildren;
-}
+const layoutBlogPost = ({ frontMatter, children }: PostLayoutProps) => {
+  const postMetaData = toPostMetaData(frontMatter);
+  const [prevPost, nextPost] = usePrevAndNextBlogPost(postMetaData);
 
-const layoutBlogPost = (frontMatter: PostFrontMatter) => {
-  // eslint-disable-next-line react/display-name
-  return ({ children }: PostProps) => {
-    const postMetaData = toPostMetaData(frontMatter);
-    const [prevPost, nextPost] = usePrevAndNextBlogPost(postMetaData);
-
-    return (
-      <>
-        <SeoBlogPost blogPost={postMetaData} />
-        <ResponsiveContainer>
-          <Column>
-            <MarginLarge />
-            <h2>{postMetaData.title}</h2>
-            <TextMuted>{postMetaData.date}</TextMuted>
-            <MarginMedium />
-            {children}
-            <MarginLarge />
-            {(nextPost || prevPost) && (
-              <>
-                <Divider />
-                <MarginMedium />
-                <h2>Mer innehåll</h2>
-                <ResponsiveGrid itemWidth="250px" gutter={theme.margins.large}>
-                  {prevPost && <CardPost subHeader="&larr; Föregående" post={prevPost} />}
-                  {nextPost && <CardPost subHeader="Nästa &rarr;" post={nextPost} />}
-                </ResponsiveGrid>
-              </>
-            )}
-          </Column>
-        </ResponsiveContainer>
-      </>
-    );
-  };
+  return (
+    <>
+      <SeoBlogPost blogPost={postMetaData} />
+      <ResponsiveContainer>
+        <Column>
+          <MarginLarge />
+          <h2>{postMetaData.title}</h2>
+          <TextMuted>{postMetaData.date}</TextMuted>
+          <MarginMedium />
+          {children}
+          <MarginLarge />
+          {(nextPost || prevPost) && (
+            <>
+              <Divider />
+              <MarginMedium />
+              <h2>Mer innehåll</h2>
+              <ResponsiveGrid itemWidth="250px" gutter={theme.margins.large}>
+                {prevPost && <CardPost subHeader="&larr; Föregående" post={prevPost} />}
+                {nextPost && <CardPost subHeader="Nästa &rarr;" post={nextPost} />}
+              </ResponsiveGrid>
+            </>
+          )}
+        </Column>
+      </ResponsiveContainer>
+    </>
+  );
 };
 
 export default layoutBlogPost;
