6199f5b9550a9e09ed7df2856ee991cb3e638f60
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-10-28 13:10:38 +0100

    Add scale helper that is aware of theme context

--- src/theme/helpers/scaleHelpers.ts
+++ src/theme/helpers/scaleHelpers.ts
@@ -1,8 +1,13 @@
 import { css } from 'styled-components';
+import { WithThemeContext } from '../../types/theme/ThemeSettings';
 
 export const scaleToRem = (scale: number) => `${scale}rem`;
 
 export const scaleToPx = (scale: number) => `${scale}px`;
+
+export const scaleSpacingToPx = (scale: number) => css(
+  ({ theme }: WithThemeContext) => scaleToPx(theme.spacings[scale])
+);
 
 export const scaleToMediaQuery = (scale: number) => (templateStrings: TemplateStringsArray) => css`
   @media (min-width: ${({theme}) => (theme.breakpoints[scale])}) {
