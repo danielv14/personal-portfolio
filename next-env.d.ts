c2889e0cc15de822ef5c6e707e5a3adf53b747aa
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2021-07-09 16:30:40 +0200

    trying seperate d.ts file for mdx

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,17 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module '*.mdx' {
-  import { ReactNode } from 'react';
-
-  export const frontMatter: {
-    title: string;
-    layout?: string;
-    summary: string;
-    date?: string;
-    __resourcePath: string;
-  };
-
-  const component: ReactNode;
-  export default ReactNode;
-}
