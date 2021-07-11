c517b979632a10cf4979fb9cf35e04fef7f5c0ac
Daniel Vernberg <daniel.vernberg@vkmedia.se>
2021-07-11 15:22:51 +0200

    various small changes

--- src/components/sections/sectionHero.styles.ts
+++ src/components/sections/sectionHero.styles.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
+export const TitleWrapper = styled.div`
   display: inline-block;
   transform: skewY(-4deg);
+  font-size: var(--font-size-h1);
 `;
 
-Title.displayName = 'Title';
+TitleWrapper.displayName = 'Title';
+
+export const SubTitleWrapper = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+SubTitleWrapper.displayName = 'SubTitleWrapper';
